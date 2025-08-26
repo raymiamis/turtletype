2997bd78aa62ebd0ba0ea275bc8a76f9c70acf2b
Noah Dinkel <74790871+raymiamis@users.noreply.github.com>
2025-08-26 19:02:08 +0200

    first complete version

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 const char* words[] = {
     "the", "be", "and", "of", "a",
@@ -12,11 +14,14 @@ const char* words[] = {
 
 int main() {
 
-    int correctWords;
+    printf("Timer will start as soon as the words are displayed.\nYour 10 words are:\n");
+
+    int correctWords = 0;
     int indices[30];
+    srand(time(NULL));  // setting seed for rand()
+
+        // shuffling the indices of the word table
     for (int i = 0; i < 30; i++) indices[i] = i;
-
-
     for (int i = 29; i > 0; i--) {
         int j = rand() % (i + 1);
         int tmp = indices[i];
@@ -24,19 +29,33 @@ int main() {
         indices[j] = tmp;
     }
 
-
+        // printing the words
     for (int i = 0; i < 10; i++) {
         printf("%s ", words[indices[i]]);
     }
+    printf("\n");
 
+        // timer start
+    time_t start = time(NULL);
+
+        // getting user input
     char input[256];
     if (fgets(input, sizeof(input), stdin)) {
+
+            // timer end and calculating diff
+        time_t end = time(NULL);
+        int timediff = difftime(end, start);
+
+            // deleting the newline char from the input buffer
         input[strcspn(input, "\n")];
 
+            // tokenizing the user input
         char* pToken = strtok(input, " ");
 
         int i;
         for (i = 0; i < 10 && pToken != NULL; i++) {
+
+                // comparing with words
             if (strcmp(pToken, words[indices[i]])) {
                 correctWords++;
             }
@@ -44,12 +63,20 @@ int main() {
             strtok(NULL, " ");
         }
 
+            // calculating wpm
+        if (0 == timediff) timediff++;   // handling divide by zero error hehehe
+        int wpm = (correctWords * 60) / timediff;
+
+            // output summary
         if (i < 10) {
             printf("You typed less words than given. Separate your words by spaces.\n");
+        } else {
+            printf("Congrats! You typed %d correct words with a speed of %d WPM", correctWords, wpm);
         }
-    }
 
-    
+    } else {
+        printf("Error while getting user input.");
+    }    
 
     return 0;
 }
